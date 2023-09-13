b878bed76b289891978ba328d7c11e7c0fa5cd14
Andre Proenca <andreproenca@hotmail.com>
2023-09-13 09:16:50 -0400

    Pattern Strategy

--- estudando design patterns java/src/one/digitalinnovation/gof/TesteApp.java
+++ estudando design patterns java/src/one/digitalinnovation/gof/TesteApp.java
@@ -1,9 +1,18 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
 public class TesteApp {
 	public static void main(String[] args) {
 		
-		//Testes relacionados ao Design Pattern Singleton.
+		//Singleton.
 		SingletonLazy lazy = SingletonLazy.getInstancia();
 		System.out.println(lazy);
 		lazy = SingletonLazy.getInstancia();
@@ -18,6 +27,23 @@ public class TesteApp {
 		System.out.println(lazyHolder);
 		lazyHolder = SingletonLazyHolder.getInstancia();
 		System.out.println(lazyHolder);
+		
+		//Strategy.
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);		
+		robo.mover();
+		robo.mover();
+		robo.setComportamento(defensivo);
+		robo.mover();
+		robo.setComportamento(agressivo);
+		robo.mover();
+		
+		
+		
 	}
 
 }
